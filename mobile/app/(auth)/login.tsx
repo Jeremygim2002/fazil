62ea8a05b11961dd056d89cec31ea08e7adaf76e
Jeremygim2002 <127067985+Jeremygim2002@users.noreply.github.com>
2026-05-29 15:01:21 -0500

    feat: authentication module

--- mobile/app/(auth)/login.tsx
+++ mobile/app/(auth)/login.tsx
@@ -19,7 +19,9 @@ export default function LoginScreen() {
   );
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: process.env.EXPO_PUBLIC_GOOGLE_EXPO_CLIENT_ID,
+    clientId:
+      process.env.EXPO_PUBLIC_GOOGLE_EXPO_CLIENT_ID ??
+      process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
     iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
--- server/src/auth/dto/login.dto.ts
+++ server/src/auth/dto/login.dto.ts
@@ -3,5 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class LoginDto {
   @IsString()
   @IsNotEmpty()
-  token: string;
+  token!: string;
 }
